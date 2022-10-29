f0d93f3040a797a75cbf6872fd8f81eb37b44064
Pemoca <107152636+Pemoca@users.noreply.github.com>
2022-10-29 02:16:56 -0300

    JSON no banco de dados e método criarAtividade

--- src/Templates/scripts.js
+++ src/Templates/scripts.js
@@ -8,6 +8,8 @@ function onOff(type) {
             .querySelector("#modal-atividade")
             .classList
             .toggle("addScroll")
+        limparCampos()
+        limparErros()
     }
     else if(type == "item"){
         document
@@ -32,49 +34,155 @@ function onOff(type) {
     }
 }
 
-function checkFields(event) {
+// Criar Atividade
 
-    const valuesToCheck = [
-        "titulo",
-        "data-inicio",
-        "data-fim",
-        "horario-inicio",
-        "horario-final",
-        "Categoria",
-        "Prioridade",
-    ]
+const titulo =  document.getElementById('titulo')
+const descricao =  document.getElementById('descricao')
+const dataInicio =  document.getElementById('data-inicio')
+const dataFim =  document.getElementById('data-fim')
+const horarioInicio =  document.getElementById('horario-inicio')
+const horarioFinal =  document.getElementById('horario-final')
+const categoria =  document.getElementById('categoria')
+const prioridade =  document.getElementById('prioridade')
+const periodizacao =  document.getElementById('periodizacao')
 
-    const isEmpty = valuesToCheck.find(function (value) {
-        const checkifIsString = typeof event.target[value].value === "string"
-        const checkIfIsEmpty = !event.target[value].value.trim()
-        if (checkifIsString && checkIfIsEmpty) {
-            return true
+const getDados = () => JSON.parse(localStorage.getItem("dbAtividade")) ?? [];
+const setDados = (dbAtividade) => localStorage.setItem("dbAtividade", JSON.stringify(dbAtividade));
+
+const criarAtividade = (atividade) => {
+    const dbAtividade = getDados();
+    dbAtividade.push(atividade);
+    setDados(dbAtividade)
+}
+
+const limparCampos = () => {
+    const inputs = document.querySelectorAll('form input')
+    inputs.forEach(input => input.value = '')
+
+    const textareas = document.querySelectorAll('form textarea')
+    textareas.forEach(textarea => textarea.value = '')
+
+    const selects = document.querySelectorAll('form select')
+    selects.forEach(select => select.value = '')
+}
+
+const limparErros = () => {
+    titulo.classList.remove('campo-vazio')
+    descricao.classList.remove('campo-vazio')
+    dataInicio.classList.remove('campo-vazio')
+    dataFim.classList.remove('campo-vazio')
+    horarioInicio.classList.remove('campo-vazio')
+    horarioFinal.classList.remove('campo-vazio')
+    categoria.classList.remove('campo-vazio')
+    prioridade.classList.remove('campo-vazio')
+    periodizacao.classList.remove('campo-vazio')
+}
+
+const salvarAtividade = () => {
+    var erros = 0
+    
+    if (titulo.value == '') {
+        titulo.classList.add('campo-vazio')
+        erros++
+    }
+    if (descricao.value == '') {
+        descricao.classList.add('campo-vazio')
+        erros++
+    }
+    if (dataInicio.value == '') {
+        dataInicio.classList.add('campo-vazio')
+        erros++      
+    }
+    if (dataFim.value == '') {
+        dataFim.classList.add('campo-vazio')
+        erros++        
+    }
+    if (horarioInicio.value == '') {
+        horarioInicio.classList.add('campo-vazio')
+        erros++        
+    }
+    if (horarioFinal.value == '') {
+        horarioFinal.classList.add('campo-vazio')
+        erros++        
+    }
+    if (categoria.value == '') {
+        categoria.classList.add('campo-vazio')
+        erros++    
+    }
+    if (prioridade.value == '') {
+        prioridade.classList.add('campo-vazio')
+        erros++      
+    }
+    if (periodizacao.value == '') {
+        periodizacao.classList.add('campo-vazio')
+        erros++
+    }
+    
+    if (erros == 0) {
+        const atividade = {
+            titulo: titulo.value,
+            descricao: descricao.value,
+            dataInicio: dataInicio.value,
+            dataFim: dataFim.value,
+            horarioInicio: horarioInicio.value,
+            horarioFinal: horarioFinal.value,
+            categoria: categoria.value,
+            prioridade: prioridade.value,
+            periodizacao: periodizacao.value
         }
-    })
-
-    if (isEmpty) {
-        event.preventDefault()
-        alert("Por favor, preencha todos os campos")
+        criarAtividade(atividade)
+        onOff(atividade)
     }
 }
 
-server.post("/", function (req, res) {
-    const query = `INSERT INTO ideas (image, title, category, description, url) VALUES (?,?,?,?,?);`
-    const values = [
-        req.body.image,
-        req.body.title,
-        req.body.category,
-        req.body.description,
-        req.body.url
-    ]
+document.getElementById('criar-atividade')
+    .addEventListener('click', salvarAtividade)
 
-    db.run(query, values, function (err) {
-        if (err) {
-            console.log(err)
-            return res.send("Erro no banco de dados!")
-        }
+// function checkFields(event) {
 
-        return res.redirect("/ideias")
-    })
+//     const valuesToCheck = [
+//         "titulo",
+//         "data-inicio",
+//         "data-fim",
+//         "horario-inicio",
+//         "horario-final",
+//         "Categoria",
+//         "Prioridade",
+//     ]
 
-})
+//     const isEmpty = valuesToCheck.find(function (value) {
+//         const checkifIsString = typeof event.target[value].value === "string"
+//         const checkIfIsEmpty = !event.target[value].value.trim()
+//         if (checkifIsString && checkIfIsEmpty) {
+//             return true
+//         }
+//     })
+
+//     if (isEmpty) {
+//         event.preventDefault()
+//         alert("Por favor, preencha todos os campos")
+//     }
+// }
+
+
+
+// server.post("/", function (req, res) {
+//     const query = `INSERT INTO ideas (image, title, category, description, url) VALUES (?,?,?,?,?);`
+//     const values = [
+//         req.body.image,
+//         req.body.title,
+//         req.body.category,
+//         req.body.description,
+//         req.body.url
+//     ]
+
+//     db.run(query, values, function (err) {
+//         if (err) {
+//             console.log(err)
+//             return res.send("Erro no banco de dados!")
+//         }
+
+//         return res.redirect("/ideias")
+//     })
+
+// })
