c5bab7343c413e910073f91fa3f8cd6e11d68d9d
Juudm <114538749+Juudm@users.noreply.github.com>
2022-11-01 22:37:16 -0300

    Edições no css

--- src/Templates/template-cores-tina.js
+++ src/Templates/template-cores-tina.js
@@ -1,1 +1,19 @@
+function initThemeSelector() {
+    const themeSelect = document.getElementById("themeSelect");
+    const themeStylesheetLink = document.getElementById("themeStylesheetLink");
+    const currentTheme = localStorage.getItem("theme") || "tema-padrao";
 
+    function ativarTema(themeName) {
+        themeStylesheetLink.setAttribute("href", `temas/${themeName}.css`);
+    }
+    themeSelect.addEventListener("change", () => {
+        ativarTema(themeSelect.value);
+        localStorage.setItem("theme", themeSelect.value);
+    });
+
+    ativarTema(currentTheme);
+    themeSelect.value = currentTheme;
+
+}
+
+initThemeSelector();
