6a258b3cdcdd228febddc80655b2319e8f0d0ec2
ellen-goncalves <ellenctgoncalves@gmail.com>
2022-11-27 23:43:05 -0300

    Corrigir conflitos

--- scripts.js
+++ scripts.js
@@ -1,55 +1,26 @@
-function onOff(type) {
+function onOff(type, action = null) {
     if (type == "atividade") {
-        document
-            .querySelector("#modal-atividade")
-            .classList
-            .toggle("hide")
-        document
-            .querySelector("#modal-atividade")
-            .classList
-            .toggle("addScroll")
+        document.querySelector("#modal-atividade").classList.toggle("hide")
+        document.querySelector("#modal-atividade").classList.toggle("addScroll")
         limparCampos()
         limparErros()
     }
-    else if (type == "item"){
-        document
-            .querySelector("#modal-item")
-            .classList
-            .toggle("hide")
-        document
-            .querySelector("#modal-item")
-            .classList
-            .toggle("addScroll")
-        }
-
-    else if (type == "habito") {
-        document
-            .querySelector("#modal-habito")
-            .classList
-            .toggle("hide")
-        document
-            .querySelector("#modal-habito")
-            .classList
-            .toggle("addScroll")
-    } else if (type == "atualizar-atividade") {
-        document
-            .querySelector("#modal-atualizar-atividade")
-            .classList
-            .toggle("hide")
-        document
-            .querySelector("#modal-atualizar-atividade")
-            .classList
-            .toggle("addScroll")
-        limparErrosEdicao()
+    else if (type == "item") {
+        document.querySelector("#modal-item").classList.toggle("hide")
+        document.querySelector("#modal-item").classList.toggle("addScroll")
+        limparCampos()
+        limparErros()
+        mostrarBotoes(action)
     }
-    else if (type == "categoria"){
-        document
-            .querySelector("#modal-categoria")
-            .classList
-            .toggle("hide")
-        document
-            .querySelector("#modal-categoria")
-            .classList
-            .toggle("addScroll")
+    else if (type == "habito") {
+        document.querySelector("#modal-habito").classList.toggle("hide")
+        document.querySelector("#modal-habito").classList.toggle("addScroll")
+        limparCampos()
+        limparErros()
+        mostrarBotoesHabito(action)
+    }
+    else if (type == "categoria") {
+        document.querySelector("#modal-categoria").classList.toggle("hide")
+        document.querySelector("#modal-categoria").classList.toggle("addScroll")
     }
 }
