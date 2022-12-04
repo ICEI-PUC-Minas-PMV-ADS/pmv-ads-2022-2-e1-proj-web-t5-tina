a396170b1e8d9429c8130265d312dd66ff3d418e
Juudm <114538749+Juudm@users.noreply.github.com>
2022-12-03 23:22:07 -0300

    Modificações nos arquivos da tela perfil

--- src/Templates/perfil.js
+++ src/Templates/perfil.js
@@ -1,20 +1,63 @@
-function exibeUsuarios() {
-    
-    document.querySelector("#nome").value = usuarioCorrente.nome
-    document.querySelector("#email").value = usuarioCorrente.email
-}
+//   Verifica se o usuário já esta logado e se negativo, redireciona para tela de login        
+//   if (!usuarioCorrente.login) {
+//     window.location.href = login.html;
+// }
+document.querySelector("#nomePerfil").value = usuarioCorrente.nome
+document.querySelector("#emailPerfil").value = usuarioCorrente.email
+document.querySelector("#senhaPerfil").value = usuarioCorrente.senha
+
+let nomePerfil = usuarioCorrente.nome
+let emailPerfil = usuarioCorrente.email
+let senhaPerfil = usuarioCorrente.senha
+
+console.log(nomePerfil)
 
 function initPage() {
     // Associa a função de logout ao botão
     document.getElementById('btn_logout').addEventListener('click', logoutUser);
-    // Lista os usuários 
-    exibeUsuarios ();
 }
 
 // Associa ao evento de carga da página a função para verificar se o usuário está logado
 window.addEventListener('load', initPage);
 
-//foto
+///tentativa de editar cadastro ----------------------------------
+
+function editDados (event) {
+    event.preventDefault();
+
+    var usuariosJSON = localStorage.getItem('db_usuarios');
+    db_usuarios = JSON.parse(usuariosJSON);    
+
+    for (var i = 0; i < db_usuarios.usuarios.length; i++) {
+        var usuario = db_usuarios.usuarios[i];
+     }
+
+let nome = document.getElementById('nomePerfil').value;
+let email = document.getElementById('emailPerfil').value;
+let senha = document.getElementById('senhaPerfil').value;
+let senhaNova = document.getElementById('confirmSenha').value;
+
+if (senha != senhaNova) {
+    alert("as senhas não são iguais, tente novamente")
+    } else if ((nome!= nomePerfil) || (email!= emailPerfil) || (senha!= senhaPerfil)) {
+    usuario.nome = nome
+    usuario.email = email
+    usuario.senha = senha
+    localStorage.setItem('db_usuarios', JSON.stringify (db_usuarios));
+
+    usuarioCorrente.email = usuario.email;
+    usuarioCorrente.senha = usuario.senha;
+    usuarioCorrente.nome = usuario.nome;        
+    localStorage.setItem('usuarioCorrente', JSON.stringify (usuarioCorrente));
+    sessionStorage.setItem('usuarioCorrente', JSON.stringify (usuarioCorrente));
+    
+    alert("Alterações salvas com sucesso :)")
+} 
+}
+
+document.getElementById('salvaAteracoes').addEventListener('click', editDados);
+
+//foto,
 document.querySelector('#image_input').addEventListener('change', function () {
     const reader = new FileReader()
 
@@ -30,6 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (recentImageDataUrl) {
         document.querySelector('#imgPreview').setAttribute('src', recentImageDataUrl)
     }
+
 })
 
 //recarrega foto
@@ -37,6 +81,7 @@ document.querySelector('#salvaFoto').addEventListener('click', function recarreg
     window.location.reload();
 } )
 
+//Fazer logout
 function logoutUser () {
     usuarioCorrente = {};
     sessionStorage.setItem ('usuarioCorrente', JSON.stringify (usuarioCorrente));
