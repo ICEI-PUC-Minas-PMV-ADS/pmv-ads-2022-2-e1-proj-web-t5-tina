cecbefeabbde2159661a59bc74bbcd3c9dbcbb32
Pemoca <107152636+Pemoca@users.noreply.github.com>
2022-12-04 17:44:34 -0300

    ajuste horarios

--- src/Templates/calendarioDiario.js
+++ src/Templates/calendarioDiario.js
@@ -5,6 +5,56 @@ const calendar = document.getElementById("calendar");
 const diaSemana = document.getElementById("dia-semana");
 const weekday = document.createElement("div");
 let diaDaSemana = "";
+const intervaloHorarios = [
+  "00:00",
+  "00:30",
+  "01:00",
+  "01:30",
+  "02:00",
+  "02:30",
+  "03:00",
+  "03:30",
+  "04:00",
+  "04:30",
+  "05:00",
+  "05:30",
+  "06:00",
+  "06:30",
+  "07:00",
+  "07:30",
+  "08:00",
+  "08:30",
+  "09:00",
+  "09:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00",
+  "18:30",
+  "19:00",
+  "19:30",
+  "20:00",
+  "20:30",
+  "21:00",
+  "21:30",
+  "22:00",
+  "22:30",
+  "23:00",
+  "23:30",
+];
 
 function load() {
   const dt = new Date();
@@ -27,15 +77,22 @@ function load() {
   const daySquare = document.createElement("div");
   daySquare.classList.add("dayDiario");
 
-  const dayString = `${year}-${month < 9 ? "0" : ""}${month + 1}-${
-    day < 10 ? "0" : ""
-  }${day}`;
-
   daySquare.innerText = day;
 
   // inserirAtividades(dayString, daySquare);
 
   daySquare.id = "currentDayDiario";
+
+  const horarios = document.createElement("div");
+  horarios.id = "horarios";
+
+
+  intervaloHorarios.forEach(horario => {
+    const intervalo = document.createElement("span");
+    intervalo.innerText = horario
+    horarios.appendChild(intervalo);
+  });
+
   weekday.classList.add("weekday");
   diaDaSemana = dt.toLocaleDateString("pt-br", {
     weekday: "long",
@@ -45,6 +102,7 @@ function load() {
 
   calendar.appendChild(daySquare);
   diaSemana.appendChild(weekday);
+  daySquare.appendChild(horarios);
 }
 
 function initButtons() {
