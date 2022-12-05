60051949b0c42601f24bffdb6006efc2b047d620
Pemoca <107152636+Pemoca@users.noreply.github.com>
2022-12-04 21:33:42 -0300

    Calendário Diário

---
+++ src/Templates/inserirAtividadeDiario.js
@@ -0,0 +1,37 @@
+function inserirAtividades(dayString, intervalo, idIntervalo) {
+  getDados().forEach((atividade) => {
+    if (atividade.dataInicio === dayString && getDados() != []) {
+      const eventDiv = document.createElement("div");
+      eventDiv.classList.add("event");
+
+      // Alterando a cor da atividade de acordo com a categoria
+      // pegos os dados salvo no LocalStorage e comparo o nome da categoria com a categoria declarada na atividade, depois checo qual a cor e aplico a estilização
+      const getLocalStorage = () =>
+        JSON.parse(localStorage.getItem("db_categoria")) ?? [];
+      getLocalStorage().forEach((categoria) => {
+        if (atividade.categoria == categoria.nome) {
+          if (categoria.cor == "Azul") eventDiv.classList.add("blueText");
+          else if (categoria.cor == "Amarelo")
+            eventDiv.classList.add("yellowText");
+          else if (categoria.cor == "Vermelho")
+            eventDiv.classList.add("redText");
+          else if (categoria.cor == "Verde")
+            eventDiv.classList.add("greenText");
+          else if (categoria.cor == "Rosa") eventDiv.classList.add("pinkText");
+        }
+      });
+
+      eventDiv.setAttribute("id", atividade.titulo);
+      eventDiv.innerText = atividade.titulo;
+
+      const [horas, minutos] = atividade.horarioInicio.split(":");
+      const [horasIntervalo, minutosIntervalo] = idIntervalo.split(":");
+      if (
+        horasIntervalo == horas &&
+        parseInt(minutos) >= parseInt(minutosIntervalo)
+      ) {
+        intervalo.appendChild(eventDiv);
+      }
+    }
+  });
+}
